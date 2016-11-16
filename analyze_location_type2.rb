4c6fd02f71ba0bc190cf1dade82a8ebb02f2b3bd
QichiroTani <tani@solairo.co.jp>
2016-11-16 19:02:04 +0900

    Unix Timeによる時間計算に変更

--- analyze_location_type2.rb
+++ analyze_location_type2.rb
@@ -21,49 +21,57 @@ require 'csv'
 require 'time'
 
 
-location_data = CSV.read('locaton_data.csv', headers: true)
+location_data = CSV.read('../locaton_data.csv', headers: true)
 
-locaton_base_data = Array.new
+location_base_data = []
 
 #レコード整形
 location_data.each do |location_set|
-  location = Array.new
+  location = []
   location << location_set[2] #uuid
-  location << location_set[6] #Location
-  location << Time.parse(location_set[0]) #Date
-  locaton_base_data << location
+  location << location_set[7] #Location_ID
+  #location << Time.parse(location_set[0]) #Date
+  location << Time.at(location_set[5].to_i/1000.0)
+  location << location_set[5] #Unix Time
+  #location << Time.at(location_set[5].to_i/1000.0).strftime('%Y/%m/%d %H:%M:%S') #Cast Unix Time to Date
+  location_base_data << location
 end
+p location_base_data[0]
 
-puts "読込レコード数 : #{locaton_base_data.length}件"
+puts "読込レコード数 : #{location_base_data.length}件"
 
 #-------------------Make Location Traking sets--------------------------------
 puts "パース開始"
-uuid_tracking_sets = Array.new
+uuid_tracking_sets = []
 uuid_tracking_set = {}
 
 
 tmp_uuid = ""                                     #一つ前のUUIDをストアする一時変数
-tmp_date = locaton_base_data[0][2]                #一つ前の時間をストアする一時変数(一番最初のレコードの時間を初期値としてセット)
+p tmp_date = location_base_data[0][2]                #一つ前の時間をストアする一時変数(一番最初のレコードの時間を初期値としてセット)
+#tmp_date = location_base_data[0][3]
 #Location情報セット検索ループ
-locaton_base_data.each do |data|
+location_base_data.each do |data|
   uuid_tracking_set["uid"] = data[0]              #現UUIDを入力
+  uuid_tracking_set["unix_time"] = data[3]        #現時間を入力
   uuid_tracking_set["1stdate"] = data[2]          #現時間を入力
-  uuid_tracking_set["locat_no"] = data[1].to_i    #現URL IDを入力
+  uuid_tracking_set["locat_no"] = data[1]    #現URL IDを入力
   uuid_tracking_set["cont_flg"] = 0               #連続閲覧状態を0でセット(連続する場合1/単体又は最初の場合0)
 
   #時差計算
   time_deffe = uuid_tracking_set["1stdate"] - tmp_date
+  #time_deffe = uuid_tracking_set["unix_time"] - tmp_date
   days = time_deffe.divmod(24*60*60)
   hours = days[1].divmod(60*60)
   mins = hours[1].divmod(60)
 
-  if tmp_uuid == uuid_tracking_set["uid"] && mins[0] <= 10  #uuidとlocation_uuidが連続で任意の時間(10分)以内の場合
+  #if tmp_uuid == uuid_tracking_set["uid"] && mins[0] <= 10  #uuidとlocation_uuidが連続で任意の時間(10分)以内の場合
+  if tmp_uuid == uuid_tracking_set["uid"] && mins[0] <= 10
     #アクセスに任意以内の空き時間がある場合別の連続閲覧扱いにする処理
     uuid_tracking_set["cont_flg"] = 1
   else
     tmp_date = uuid_tracking_set["1stdate"]       #時間更新(現時間を初見時間と判定)
     uuid_tracking_set["cont_flg"] = 0             #先頭URLとして0へ変更
-  end #uuidとlocation_uuidが合う場合END
+  end #uuidとlocation_uuidが連続で任意の時間(10分)以内の場合 END
   tmp_uuid = data[0]
   uuid_tracking_sets << uuid_tracking_set
   uuid_tracking_set = {}
@@ -74,7 +82,7 @@ puts "処理したレコード数 : #{uuid_tracking_sets.length}件"
 
 #書き出し
 set_array = []
-CSV.open("Location_traking_sets_ver3.csv",'wb') do |csv|
+CSV.open("../Location_traking_sets_ver3.csv",'wb') do |csv|
   uuid_tracking_sets.each do |set|
     set_array = set.values
     #puts "#{set_array}"
